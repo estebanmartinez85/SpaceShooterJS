9ef3b3ad31e022e89acebfbdc545b6138c69cabd
esteban.martinez <esteban.martinez@outlook.com>
2018-08-08 23:26:49 -0500

    Added Game Loop, Keyboard, and Imports

---
+++ js/keyboard.js
@@ -0,0 +1,46 @@
+export const Keyboard = (()=>{
+
+    return {
+        Up: false,
+        Right: false,
+        Down: false,
+        Left: false,
+        registerKeys: function(canvas){
+            canvas.onkeydown = function(e){
+                e = e || window.event;
+                switch (e.key){
+                    case "w":
+                        Keyboard.Up = true;
+                        break;
+                    case "d":
+                        Keyboard.Right = true;
+                        break;
+                    case "s":
+                        Keyboard.Down = true;
+                        break;
+                    case "a":
+                        Keyboard.Left = true;
+                        break;
+                }
+
+            };
+            canvas.onkeyup = function(e){
+                e = e || window.event;
+                switch (e.key){
+                    case "w":
+                        Keyboard.Up = false;
+                        break;
+                    case "d":
+                        Keyboard.Right = false;
+                        break;
+                    case "s":
+                        Keyboard.Down = false;
+                        break;
+                    case "a":
+                        Keyboard.Left = false;
+                        break;
+                }
+            };
+        }
+    }
+})();
--- js/main.js
+++ js/main.js
@@ -1,32 +1,73 @@
+import { Keyboard } from "./keyboard.js";
+
+const Player = (()=>{
+   return {
+
+       x: 100,
+       y: 100
+   }
+});
+
 const Main = (function() {
-
-    const canvas = document.querySelector("#Canvas");
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = 'rgb(200,0,0)';
-    ctx.fillRect(20,20,20,20);
-
-    ctx.font = "30px Arial";
-    ctx.fillText("Press Enter", 200,200);
-
-    if(gl == null)
-    {
-        alert("WebGL not found.");
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
     }
 
-    function p(){
-        return "private";
-    };
+    let canvas = document.getElementById('Canvas'),
+        cw = canvas.width,
+        ch = canvas.height,
+        cx = null,
+        fps = 60,
+        bX = 30,
+        bY = 30,
+        mX = 10,
+        mY = 20,
+        interval     =    1000/fps,
+        lastTime     =    (new Date()).getTime(),
+        currentTime  =    0,
+        delta = 0;
 
-    return {
-        pub: function() {
-            return "public";
-        },
-        test: function(){
-            return p();
+    var x = 100;
+    var y = 100;
+
+    Keyboard.registerKeys(canvas);
+
+    function gameLoop() {
+        window.requestAnimationFrame(gameLoop);
+
+        currentTime = (new Date()).getTime();
+        delta = (currentTime-lastTime);
+
+        if(delta > interval) {
+            cx.clearRect(0,0,cw,ch);
+
+            if(Keyboard.Up){
+                y -= 10;
+            }
+            if(Keyboard.Right){
+                x += 10;
+            }
+            if(Keyboard.Down){
+                y += 10;
+            }
+            if(Keyboard.Left){
+                x -= 10;
+            }
+            cx.drawImage(img, x,y);
+
+            lastTime = currentTime - (delta % interval);
         }
     }
 
+    var img = new Image(32,32);
+    img.src = "/SpaceShooterJS/img/PlayerShip.png";
+    img.addEventListener("load", () => {
+        if (typeof (canvas.getContext) !== undefined) {
+            cx = canvas.getContext('2d');
+            gameLoop();
+        }
+    }, false);
 })();
-
-console.log(Main.test());
